612733911eb5a6015da7d9a504614c62e1ca4dd9
wangchaoqun <chaoqun1166@163.com>
2020-04-24 18:35:31 +0800

    吧本地工厂添加到git

---
+++ git——test/src/com/cq/git/git_Test.java
@@ -0,0 +1,11 @@
+package com.cq.git;
+
+public class git_Test {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+        System.out.println("hello.git");
+        System.out.println("hello.git");
+        System.out.println("hello.git");
+        System.out.println("hello.git");
+    }
+}
